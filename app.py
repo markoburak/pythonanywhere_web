291008d97d82711ac4c10a2fa639d80ac2c0db3c
markoburak <burakmarko@gmail.com>
2023-07-02 14:03:34 -0400

    added main endpoint

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
 @app.route('/')
 def hello_world():
@@ -15,3 +16,7 @@ def test():
 @app.route('/test2')
 def test2():
     return 'Hello test Teodor here!'
+
+@app.route("/main")
+def index():
+    return render_template("main_page.html")
