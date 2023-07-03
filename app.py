2f96bde1b68bf8fbaf8526eeccc0cd172756897c
markoburak <burakmarko@gmail.com>
2023-07-03 19:00:43 -0400

    added main endpoint to retrieve and save comments, on local session

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
+
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/')
+@app.route("/")
 def hello_world():
     return 'Hello Marko here!'
 
@@ -17,6 +18,11 @@ def test():
 def test2():
     return 'Hello test Teodor here!'
 
-@app.route("/main")
+comments = []
+@app.route("/main", methods=["GET", "POST"])
 def index():
-    return render_template("main.html")
+    if request.method == "GET":
+        return render_template("main.html", comments=comments)
+    elif request.method == "POST":
+        comments.append(request.form["contents"])
+    return redirect(url_for('index'))
