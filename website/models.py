446374e025d66850462b458a0342cecc2e1bf4a2
markoburak <burakmarko@gmail.com>
2023-08-09 20:35:09 -0400

    added a delete function for test_users and created test_users in general

--- website/models.py
+++ website/models.py
@@ -6,3 +6,13 @@ class Comment(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4096))
+
+
+class User_testing(db.Model):
+    __tablename__ = "user_testing"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(512))
+    lastname = db.Column(db.String(512))
+    email = db.Column(db.String(512))
+    age = db.Column(db.Integer())
--- website/views.py
+++ website/views.py
@@ -1,22 +1,10 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-from .models import Comment
+from .models import Comment, User_testing
 from . import db
 
 views = Blueprint('views', __name__)
 
-@views.route("/")
-def hello_world():
-    return 'Hello Marko here!'
-
-@views.route('/test')
-def test():
-    return 'Hello test Marko here!'
-
-@views.route('/test2')
-def test2():
-    return 'Hello test Teodor here!'
-
-@views.route("/main", methods=["GET", "POST"])
+@views.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
         comments = Comment.query.all()
@@ -26,3 +14,35 @@ def index():
         db.session.add(new_comment)
         db.session.commit()
     return redirect(url_for('views.index'))
+
+@views.route("/user_info_all", methods=["GET"])
+def user_info_all():
+    if request.method == "GET":
+        user_info_all = User_testing.query.all()
+        return render_template("user_info_all.html", user_info_all=user_info_all)
+@views.route("/user_info/<user_id>", methods=["GET", "POST"])
+def user_info(user_id):
+    user_info_data = User_testing.query.filter_by(id=user_id).first()
+    if request.method == "GET":
+        return render_template("user_info.html", user_info=user_info_data)
+    elif request.method == "POST":
+        user_info_data.name = request.form["name"]
+        user_info_data.lastname = request.form["lastname"]
+        user_info_data.email = request.form["email"]
+        user_info_data.age = request.form["age"]
+        try:
+            db.session.commit()
+            return render_template("user_info.html", user_info=user_info_data)
+        except:
+            return redirect(url_for('views.user_info'))
+
+@views.route("/user_info_delete/<user_id>", methods=["POST"])
+def user_info_delete(user_id):
+    if request.method == "POST":
+        user_info_data = User_testing.query.filter_by(id=user_id).first()
+        try:
+            db.session.delete(user_info_data)
+            db.session.commit()
+            return redirect(url_for('views.user_info_all'))
+        except:
+            return redirect(url_for('views.index'))
